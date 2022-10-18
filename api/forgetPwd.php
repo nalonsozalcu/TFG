33dc3e50417e9c0d8e95103a46a1f0bf6f2433d0
Luis Pecos Sánchez <luispecos0712@gmail.com>
2022-10-18 21:35:25 +0200

    fix(page "forgetPwd"): añadir "enviar correo con nueva contraseña"
    Se han añadido a la carpeta api/PHPMailer varios archivos necesarios para el envío de el correo de cambio de contraseña olvidada

--- api/forgetPwd.php
+++ api/forgetPwd.php
@@ -1,5 +1,13 @@
 <?php
 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../api/PHPMailer/Exception.php';
+require '../api/PHPMailer/PHPMailer.php';
+require '../api/PHPMailer/SMTP.php';
+
 require_once '../config.php';
 
 $email = isset($_POST["email"]) ? $_POST["email"] : null;
@@ -11,9 +19,38 @@ if ($usuario) {
 	$usuario->update();
 }
 
-if (isset($_POST['email'])) {
-	mail($email, 'Recuperación de contraseña', 'Su nueva contraseña es:' . $contraseña);
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'neverainteligente2022@gmail.com';                     //SMTP username
+    $mail->Password   = 'ycegjhxhpaimbhgo';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('neverainteligente2022@gmail.com', 'MadWays');
+    $mail->addAddress($email, );     //Add a recipient
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Recuperacion de contrasena';
+    $mail->Body    = 'Se ha generado una nueva contraseña para que puedas iniciar sesión con tu correo en la aplicación de MadWays. <br><br>' . 'Su nueva contraseña es:  ' . $contraseña;
+    //$mail->AltBody = 'Se ha generado una nueva contraseña para que puedas iniciar sesión con tu correo en la aplicación de MadWays';
+
+    $mail->send();
 	header("location: ../dashboard/index.php");
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
 
 ?>
