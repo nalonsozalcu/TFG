eda4b5fcaf35ee72973dd692ea364cbab83b1647
nalonsozalcu <100678422+nalonsozalcu@users.noreply.github.com>
2022-10-20 21:12:47 +0200

    fix(head): head files in new html

--- dashboard/signupPage.php
+++ dashboard/signupPage.php
@@ -1,24 +1,7 @@
 <!DOCTYPE html>
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	
-
-	<!--     Fonts and icons     -->
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-	
-	<!-- CSS Files -->
-	<link rel="stylesheet" href="../assets/css/main.css">
-
-	<title>PlanWays</title>
-	<link rel="icon" type="image/png" href="../assets/img/favicon.png">
-
-</head>
+<?php
+	require_once "../includes/head.html";
+?>
 
 <body>
 	<!-- ENCABEZADO -->
