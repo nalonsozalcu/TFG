d9f743768f0a67f3e6037fcdeb0b9a5864c78603
Luis Pecos Sánchez <luispecos0712@gmail.com>
2022-10-26 14:46:58 +0200

    feat(eventos form): nuevo formulario para subir eventos

---
+++ dashboard/admin/forms/eventosForm.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<form class="form mb-3 mt-md-2" method="POST" action="../api/evento.php">
+	<h5 class="mb-5">Formulario de evento</h5>
+	<div class="container">
+		<div class="row mb-3">
+			<div class="col">
+				<div class="form-group">
+					<label for="nombre" class="form-label ">Nombre</label>
+					<input
+						type="text"
+						id="nombre"
+						name="nombre"
+						class="form-control"
+						placeholder="nombre"
+						required
+					/>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-group">
+					<label for="fecha_fin" class="form-label ">Fecha de fin</label>
+					<input
+						type="date"
+						id="fecha_fin"
+						name="fecha_fin"
+						class="form-control"
+						placeholder="06/12/2000"
+						required
+					/>
+				</div>
+			</div>
+            <div class="col">
+				<div class="form-group">
+					<label for="fecha_ini" class="form-label ">Fecha de inicio</label>
+					<input
+						type="date"
+						id="fecha_ini"
+						name="fecha_ini"
+						class="form-control"
+						placeholder="02/11/2000"
+						required
+					/>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-group">
+						<select class="form-select" for="categoria" aria-label="Default select example">
+							<option selected>Selecciona una categoria</option>
+							<option value="1">Musica</option>
+							<option value="2">Deportivo</option>
+							<option value="3">Three</option>
+						</select>
+				</div>
+			</div>
+		</div>
+		<div class="row mb-3">
+			<div class="col">
+				<div class="form-group">
+					<label for="descripcion" class="form-label ">Descripción</label>
+					<textarea class="form-control" id="descripcion" name="descripcion" rows="3"></textarea>
+				</div>
+			</div>
+		</div>
+        <div class="row mb-3">
+        <div class="col">
+				<div class="form-group">
+					<label for="precio" class="form-label ">Precio</label>
+					<input
+						type="text"
+						id="precio"
+						name="precio"
+						class="form-control"
+						placeholder="Precio"
+						required
+					/>
+				</div>
+			</div>
+            <div class="col">
+				<div class="form-group">
+					<label for="gratis" class="form-label ">Gratis</label>
+					<input
+						type="text"
+						id="gratis"
+						name="gratis"
+						class="form-control"
+						placeholder="gratis"
+						required
+					/>
+				</div>
+			</div>
+            <div class="col">
+				<div class="form-group">
+					<label for="audiencia" class="form-label ">Audiencia</label>
+					<input
+						type="text"
+						id="audiencia"
+						name="audiencia"
+						class="form-control"
+						placeholder="audiencia"
+						required
+					/>
+				</div>
+                <div class="col">
+				<div class="form-group">
+					<label for="dias" class="form-label ">Dias de duracion</label>
+					<input
+						type="text"
+						id="dias"
+						name="dias"
+						class="form-control"
+						placeholder="dias"
+						required
+					/>
+				</div>
+			</div>
+            <div class="col">
+				<div class="form-group">
+					<label for="dias_ex" class="form-label ">Dias restantes</label>
+					<input
+						type="text"
+						id="dias_ex"
+						name="dias_ex"
+						class="form-control"
+						placeholder="dias"
+						required
+					/>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="row mb-3">
+			<div class="col">
+				<div class="form-group">
+					<label for="horario" class="form-label ">Horario</label>
+					<input
+						type="text"
+						id="horario"
+						name="horario"
+						class="form-control"
+						placeholder="horario"
+					/>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-group">
+					<label for="transporte" class="form-label ">Transporte</label>
+					<input
+						type="text"
+						id="transporte"
+						name="transporte"
+						class="form-control"
+						placeholder="transporte"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="row mb-3">
+			<div class="col">
+				<div class="form-group">
+					<label for="url" class="form-label ">URL</label>
+					<input
+						type="text"
+						id="url"
+						name="url"
+						class="form-control"
+						placeholder="url"
+					/>
+				</div>
+			</div>
+            <div class="col">
+				<div class="form-group">
+					<label for="telefono" class="form-label ">Telefono</label>
+					<input
+						type="text"
+						id="telefono"
+						name="telefono"
+						class="form-control"
+						placeholder="Telefono"
+					/>
+				</div>
+			</div>
+            <div class="col">
+				<div class="form-group">
+					<label for="inputEmail" class="form-label ">Email</label>
+					<input
+						type="text"
+						id="inputEmail"
+						name="email"
+						class="form-control"
+						placeholder="ejemplo@gmail.com"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="row mb-3">
+			<div class="col-6">
+				<div class="form-group">
+					<label for="direccion" class="form-label ">Dirección</label>
+					<input
+						type="text"
+						id="direccion"
+						name="direccion"
+						class="form-control"
+						placeholder="direccion"
+						required
+					/>
+				</div>
+			</div>
+            <div class="col">
+				<div class="form-group">
+					<label for="lugar" class="form-label ">Lugar</label>
+					<input
+						type="text"
+						id="lugar"
+						name="lugar"
+						class="form-control"
+						placeholder="lugar"
+
+					/>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-group">
+					<label for="codpostal" class="form-label ">Codigo postal</label>
+					<input
+						type="text"
+						id="codpostal"
+						name="codpostal"
+						class="form-control"
+						placeholder="09400"
+
+					/>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-group">
+					<label for="latitud" class="form-label ">Latitud</label>
+					<input
+						type="text"
+						id="latitud"
+						name="latitud"
+						class="form-control"
+						placeholder="38.889722"
+					/>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-group">
+					<label for="longitud" class="form-label ">Longitud</label>
+					<input
+						type="text"
+						id="longitud"
+						name="longitud"
+						class="form-control"
+						placeholder="-38.889722"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="row mb-3">
+			<div class="col">
+				<div class="form-group">
+					<label for="desc_sitio" class="form-label ">Descripción del lugar</label>
+					<textarea class="form-control" id="desc_sitio" name="desc_sitio" rows="3"></textarea>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-end">
+			<div class="col-1">
+				<button class="btn btn-primary" type="submit">Guardar</button>
+			</div>
+		</div>
+	</div>
+</form>
