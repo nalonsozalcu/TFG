b057c108b0493f3cc163ba4fdc4f60f2ba2b778a
nalonsozalcu <100678422+nalonsozalcu@users.noreply.github.com>
2022-10-28 15:03:34 +0200

    fix(massive data page): new input for submit csv

---
+++ api/upload.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../config.php';
+
+$type = $_GET['type'];
+$data_file = isset($_FILES['data']) ? $_FILES["data"] : null;
+
+if($data_file && $data_file['name']!= ""){
+
+    $file_name = $data_file['name'];
+    $file_size =$data_file['size'];
+    $file_tmp =$data_file['tmp_name'];
+    $file_type=$data_file['type'];
+    $file_ext=strtolower(end(explode('.',$file_name)));
+
+    $extensions= array("csv");
+
+    if(in_array($file_ext,$extensions)=== false){
+        header("location: ../dashboard/adminPage.php?content=up_massive&type=$type&ext=false");
+        exit(1);
+    }
+
+    if($file_size > 2097152){
+        header("location: ../dashboard/adminPage.php?content=up_massive&type=$type&tam=false");
+        exit(1);
+    }
+
+    $dir="../files/data/";
+
+    mkdir($dir);
+
+    move_uploaded_file($file_tmp,$dir.$file_name);
+
+    header("location: ../dashboard/adminPage.php?content=up_massive&type=$type&ok=true");
+ }else{
+    header("location: ../dashboard/adminPage.php?content=up_massive&type=$type&file=false");
+ }
+
+
+
--- dashboard/admin/uploadMassiveData.php
+++ dashboard/admin/uploadMassiveData.php
@@ -9,11 +9,37 @@
 				<h5 class="mb-3">Museos</h5>
 				<a href="../files/plantillas/Plantilla_Museos.csv" class="btn btn-success" download="Plantilla Museos.csv">Descargar plantilla <i class="bi bi-cloud-download"></i></a>
 			</div>
-			<div class="col-4 align-self-end">
-				<input class="form-control" type="file" id="files">
-			</div>
+			<form class="form col align-self-end" method="POST" action="../api/upload.php?type=museo" enctype="multipart/form-data">
+				<div class="row">
+					<div class="col-8">
+						<input class="form-control" type="file" name="data" id="files">
+					</div>
+					<div class="col-4">
+						<button type="submit" class="btn btn-primary">Importar CSV <i class="bi bi-cloud-upload"></i></button>
+					</div>
+				</div>
+			</form>
 			<div class="col align-self-end">
-				<button type="button" class="btn btn-primary">Subir CSV <i class="bi bi-cloud-upload"></i></button>
+			<?php 
+				if(isset($_GET["type"]) && $_GET["type"]=== "museo"){
+					if(isset($_GET["ext"])){
+						if($_GET["ext"] == 'false')
+							echo ('<p class="text-danger">Extension no permitida, elija un archivo CSV.</p>');
+					} 
+					if(isset($_GET["tam"])){
+						if($_GET["tam"] == 'false')
+							echo ('<p class="text-danger">El tamaño del archivo debe ser menor de 2 MB</p>');
+					}
+					if(isset($_GET["file"])){
+						if($_GET["file"] == 'false')
+							echo ('<p class="text-danger">No has seleccionado ningun archivo.</p>');
+					}
+					if(isset($_GET["ok"])){
+						if($_GET["ok"] == 'true')
+							echo ('<p class="text-success">Se han importado los datos del museo correctamente. <i class="bi bi-check"></i></p>');
+					}
+				}
+				?>
 			</div>
 		</div>
 		<hr>
@@ -22,11 +48,37 @@
 				<h5 class="mb-3">Eventos</h5>
 				<a href="../files/plantillas/Plantilla_Eventos.csv" class="btn btn-success" download="Plantilla Eventos.csv">Descargar plantilla <i class="bi bi-cloud-download"></i></a>
 			</div>
-			<div class="col-4 align-self-end">
-				<input class="form-control" type="file" id="files">
-			</div>
+			<form class="form col align-self-end" method="POST" action="../api/upload.php?type=evento" enctype="multipart/form-data">
+				<div class="row">
+					<div class="col-8">
+						<input class="form-control" type="file" name="data" id="files">
+					</div>
+					<div class="col-4">
+						<button type="submit" class="btn btn-primary">Importar CSV <i class="bi bi-cloud-upload"></i></button>
+					</div>
+				</div>
+			</form>
 			<div class="col align-self-end">
-				<button type="button" class="btn btn-primary">Subir CSV <i class="bi bi-cloud-upload"></i></button>
+			<?php 
+				if(isset($_GET["type"]) && $_GET["type"]=== "evento"){
+					if(isset($_GET["ext"])){
+						if($_GET["ext"] == 'false')
+							echo ('<p class="text-danger">Extension no permitida, elija un archivo CSV.</p>');
+					} 
+					if(isset($_GET["tam"])){
+						if($_GET["tam"] == 'false')
+							echo ('<p class="text-danger">El tamaño del archivo debe ser menor de 2 MB</p>');
+					}
+					if(isset($_GET["file"])){
+						if($_GET["file"] == 'false')
+							echo ('<p class="text-danger">No has seleccionado ningun archivo.</p>');
+					}
+					if(isset($_GET["ok"])){
+						if($_GET["ok"] == 'true')
+							echo ('<p class="text-success">Se han importado los datos del evento correctamente. <i class="bi bi-check"></i></p>');
+						}
+				}
+				?>
 			</div>
 		</div>
 		<hr>
@@ -35,11 +87,37 @@
 				<h5 class="mb-3">Monumentos</h5>
 				<a href="../files/plantillas/Plantilla_Monumentos.csv" class="btn btn-success" download="Plantilla Monumentos.csv">Descargar plantilla <i class="bi bi-cloud-download"></i></a>
 			</div>
-			<div class="col-4 align-self-end">
-				<input class="form-control" type="file" id="files">
-			</div>
+			<form class="form col align-self-end" method="POST" action="../api/upload.php?type=monumento" enctype="multipart/form-data">
+				<div class="row">
+					<div class="col-8">
+						<input class="form-control" type="file" name="data" id="files">
+					</div>
+					<div class="col-4">
+						<button type="submit" class="btn btn-primary">Importar CSV <i class="bi bi-cloud-upload"></i></button>
+					</div>
+				</div>
+			</form>
 			<div class="col align-self-end">
-				<button type="button" class="btn btn-primary">Subir CSV <i class="bi bi-cloud-upload"></i></button>
+			<?php 
+				if(isset($_GET["type"]) && $_GET["type"]=== "monumento"){
+					if(isset($_GET["ext"])){
+						if($_GET["ext"] == 'false')
+							echo ('<p class="text-danger">Extension no permitida, elija un archivo CSV.</p>');
+					} 
+					if(isset($_GET["tam"])){
+						if($_GET["tam"] == 'false')
+							echo ('<p class="text-danger">El tamaño del archivo debe ser menor de 2 MB</p>');
+					}
+					if(isset($_GET["file"])){
+						if($_GET["file"] == 'false')
+							echo ('<p class="text-danger">No has seleccionado ningun archivo.</p>');
+					}
+					if(isset($_GET["ok"])){
+						if($_GET["ok"] == 'true')
+							echo ('<p class="text-success">Se han importado los datos del monumento correctamente. <i class="bi bi-check"></i></p>');
+					}
+				}
+				?>
 			</div>
 		</div>
 		<hr>
@@ -48,11 +126,37 @@
 				<h5 class="mb-3">Restaurantes</h5>
 				<a href="../files/plantillas/Plantilla_Restaurantes.csv" class="btn btn-success" download="Plantilla Restaurantes.csv">Descargar plantilla <i class="bi bi-cloud-download"></i></a>
 			</div>
-			<div class="col-4 align-self-end">
-				<input class="form-control" type="file" id="files">
-			</div>
+			<form class="form col align-self-end" method="POST" action="../api/upload.php?type=restaurante" enctype="multipart/form-data">
+				<div class="row">
+					<div class="col-8">
+						<input class="form-control" type="file" name="data" id="files">
+					</div>
+					<div class="col-4">
+						<button type="submit" class="btn btn-primary">Importar CSV <i class="bi bi-cloud-upload"></i></button>
+					</div>
+				</div>
+			</form>
 			<div class="col align-self-end">
-				<button type="button" class="btn btn-primary">Subir CSV <i class="bi bi-cloud-upload"></i></button>
+			<?php 
+				if(isset($_GET["type"]) && $_GET["type"]=== "restaurante"){
+					if(isset($_GET["ext"])){
+						if($_GET["ext"] == 'false')
+							echo ('<p class="text-danger">Extension no permitida, elija un archivo CSV.</p>');
+					} 
+					if(isset($_GET["tam"])){
+						if($_GET["tam"] == 'false')
+							echo ('<p class="text-danger">El tamaño del archivo debe ser menor de 2 MB</p>');
+					}
+					if(isset($_GET["file"])){
+						if($_GET["file"] == 'false')
+							echo ('<p class="text-danger">No has seleccionado ningun archivo.</p>');
+					}
+					if(isset($_GET["ok"])){
+						if($_GET["ok"] == 'true')
+							echo ('<p class="text-success">Se han importado los datos del restaurante correctamente. <i class="bi bi-check"></i></p>');
+					}
+				}
+				?>
 			</div>
 		</div>
 	</div>
