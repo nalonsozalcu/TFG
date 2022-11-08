f83c70684f96436da58619df492e5811b44f41f6
nalonsozalcu <100678422+nalonsozalcu@users.noreply.github.com>
2022-11-08 17:20:38 +0100

    minor fix

--- dashboard/contactsPage.php
+++ dashboard/contactsPage.php
@@ -36,7 +36,7 @@
 													<img class="rounded-circle" width="100px" src="../files/users/<?php echo($user["username"]) ?>/<?php echo($user["avatar"]) ?>" alt="user">
 												</div>
 												<div class="col-md-9 col-sm-9">
-													<h5><a href="#" class="profile-link"><?php echo($user["username"]) ?></a></h5>
+													<h5><a href="#" class="profile-link"><?php echo($user["nombre"]) ?></a></h5>
 													<p><?php echo($user["nombre"]) ?></p>
 													<p class="text-muted"><?php echo($user["email"]) ?></p>
 												</div>
--- dashboard/solicitudPage.php
+++ dashboard/solicitudPage.php
@@ -33,7 +33,7 @@
 												<img class="rounded-circle" width="100px" src="../files/users/<?php echo($user["username"]) ?>/<?php echo($user["avatar"]) ?>" alt="user">
 											</div>
 											<div class="col-md-6 col-sm-7">
-												<h5><a href="#" class="profile-link"><?php echo($user["username"]) ?></a></h5>
+												<h5><a href="#" class="profile-link"><?php echo($user["nombre"]) ?></a></h5>
 												<p><?php echo($user["nombre"]) ?></p>
 												<p class="text-muted"><?php echo($user["email"]) ?></p>
 											</div>
