020baa53c7fc4d984dedc85c456fef3865ed864b
nalonsozalcu <100678422+nalonsozalcu@users.noreply.github.com>
2022-11-28 19:13:40 +0100

    fix(actividades): tendencias fixed

--- dashboard/actividades/actividadesTendencias.php
+++ dashboard/actividades/actividadesTendencias.php
@@ -22,25 +22,25 @@
 						if($tendencias[$i]["tipo_actividad"] == "monumentos") {$tipo ="monumento"; $actividad = Monumento::get_monumento_by_id($tendencias[$i]["id_actividad"]);}
 						if($tendencias[$i]["tipo_actividad"] == "eventos") {$tipo ="evento"; $actividad = Evento::get_evento_by_id($tendencias[$i]["id_actividad"]);}
 						?>
-							<div class="col-4">
-								<div class="card h-100">
-									<div class="card-body">
-										<img width="60px" src="../assets/img/<?php echo($tipo."_icon.png") ?>" alt="icon">
-										<a href="actividadPage.php?content=<?php echo($tipo) ?>&id=<?php echo($actividad->id()) ?>"><h6><?php echo($actividad->nombre()) ?></h6></a>
-										<p><?php echo($actividad->direccion()) ?></p>
-										<?php 
-										if($actividad->get_global_valoracion($actividad->id())){
-											for($e=1; $e < 6; $e++){
-												if($e <= $actividad->get_global_valoracion($actividad->id())){
-													echo('<i class="bi bi-star-fill text-warning ms-1"></i>');
-												}else{
-													echo('<i class="bi bi-star-fill ms-1"></i>');
-												}
+						<div class="col-4">
+							<div class="card h-100" style="width: 25rem;">
+								<div class="card-body">
+									<img width="50px" src="../assets/img/<?php echo($tipo."_icon.png") ?>" alt="icon">
+									<a href="actividadPage.php?content=<?php echo($tipo) ?>&id=<?php echo($actividad->id()) ?>"><h6><?php echo($actividad->nombre()) ?></h6></a>
+									<p><?php echo($actividad->direccion()) ?></p>
+									<?php 
+									if($actividad->get_global_valoracion($actividad->id())){
+										for($e=1; $e < 6; $e++){
+											if($e <= $actividad->get_global_valoracion($actividad->id())){
+												echo('<i class="bi bi-star-fill text-warning ms-1"></i>');
+											}else{
+												echo('<i class="bi bi-star-fill ms-1"></i>');
 											}
-										}?>
-									</div>
+										}
+									}?>
 								</div>
 							</div>
+						</div>
 				<?php
 					endif;
 					$i++;
