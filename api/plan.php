1d54e0c85ecbe1ef477fadf76fb850881de8b256
Luis Pecos Sánchez <luispecos0712@gmail.com>
2022-12-14 20:42:33 +0100

    fix(plan.php): nuevos atributos para actividades y descripcion

--- api/plan.php
+++ api/plan.php
@@ -17,11 +17,22 @@ if($action === "delete"){
 else{
 
 $nombre = $_POST["nombre"] != "" ? $_POST["nombre"] : null;
+$descripcion = $_POST["descripcion"] != "" ? $_POST["descripcion"] : null;
+$hora_act_1 = $_POST["hora_act_1"] != "" ? $_POST["hora_act_1"] : null;
+$id_act_1 = $_POST["id_act_1"] != "" ? $_POST["id_act_1"] : null;
+$hora_act_2 = $_POST["hora_act_2"] != "" ? $_POST["hora_act_2"] : null;
+$id_act_2 = $_POST["id_act_2"] != "" ? $_POST["id_act_2"] : null;
+$hora_act_3 = $_POST["hora_act_3"] != "" ? $_POST["hora_act_3"] : null;
+$id_act_3 = $_POST["id_act_3"] != "" ? $_POST["id_act_3"] : null;
+$hora_act_4 = $_POST["hora_act_4"] != "" ? $_POST["hora_act_4"] : null;
+$id_act_4 = $_POST["id_act_4"] != "" ? $_POST["id_act_4"] : null;
+$hora_act_5 = $_POST["hora_act_5"] != "" ? $_POST["hora_act_5"] : null;
+$id_act_5 = $_POST["id_act_5"] != "" ? $_POST["id_act_5"] : null;
 $fecha = $_POST["fecha"] != "" ? $_POST["fecha"] : null;
 
 
 if($action === "new"){
-    if (Plan::registrar($nombre, $fecha))
+    if (Plan::registrar($nombre, $descripcion, $hora_act_1, $id_act_1, $hora_act_2, $id_act_2, $hora_act_3, $id_act_3, $hora_act_4, $id_act_4, $hora_act_5, $id_act_5, $fecha))
         header("location: ../dashboard/adminPage.php?content=up_indiv&ok=true");
     else
         header("location: ../dashboard/adminPage.php?content=up_indiv&ok=false");
@@ -30,6 +41,17 @@ if($action === "new"){
     
     if($action === "update"){
         if($nombre)$plan->setNombre($nombre);
+        if($descripcion)$plan->setDescripcion($descripcion);
+        if($hora_act_1)$plan->setHora_act_1($hora_act_1);
+        if($id_act_1)$plan->setId_act_1($id_act_1);
+        if($hora_act_2)$plan->setHora_act_2($hora_act_2);
+        if($id_act_2)$plan->setId_act_2($id_act_2);
+        if($hora_act_3)$plan->setHora_act_3($hora_act_3);
+        if($id_act_3)$plan->setId_act_3($id_act_3);
+        if($hora_act_4)$plan->setHora_act_4($hora_act_4);
+        if($id_act_4)$plan->setId_act_4($id_act_4);
+        if($hora_act_5)$plan->setHora_act_5($hora_act_5);
+        if($id_act_5)$plan->setId_act_5($id_act_5);
         if($fecha)$plan->setFecha($fecha);
     
         if($plan->update())
