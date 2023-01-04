d499b2c90f846eb97a2c9fb37a615799e7833826
nalonsozalcu <100678422+nalonsozalcu@users.noreply.github.com>
2023-01-04 21:20:37 +0100

    fix(modificar plan): arreglo

--- api/plan.php
+++ api/plan.php
@@ -604,29 +604,30 @@ if($action === "generate"){
 }
 
 else{
+	
 
 	$nombre = $_POST["nombre"] != "" ? $_POST["nombre"] : null;
 	$descripcion = $_POST["descripcion"] != "" ? $_POST["descripcion"] : null;
 
-	$id_act_1 = $_POST["id_1"] != "" ? $_POST["id_1"] : null;
-	$tipo_act_1 = $_POST["tipo_1"] != "" ? $_POST["tipo_1"] : null;
-	$hora_act_1 = $_POST["hora_act_1"] != "" ? $_POST["hora_act_1"] : null;
+	$id_act_1 = $_POST["id_1"];
+	$tipo_act_1 = $_POST["tipo_1"];
+	$hora_act_1 = $_POST["hora_act_1"];
 
-	$id_act_2 = $_POST["id_2"] != "" ? $_POST["id_2"] : null;
-	$tipo_act_2 = $_POST["tipo_2"] != "" ? $_POST["tipo_2"] : null;
-	$hora_act_2 = $_POST["hora_act_2"] != "" ? $_POST["hora_act_2"] : null;
+	$id_act_2 = $_POST["id_2"];
+	$tipo_act_2 = $_POST["tipo_2"];
+	$hora_act_2 = $_POST["hora_act_2"];
 
-	$id_act_3 = $_POST["id_3"] != "" ? $_POST["id_3"] : null;
-	$tipo_act_3 = $_POST["tipo_3"] != "" ? $_POST["tipo_3"] : null;
-	$hora_act_3 = $_POST["hora_act_3"] != "" ? $_POST["hora_act_3"] : null;
+	$id_act_3 = $_POST["id_3"];
+	$tipo_act_3 = $_POST["tipo_3"];
+	$hora_act_3 = $_POST["hora_act_3"];
 
-	$id_act_4 = $_POST["id_4"] != "" ? $_POST["id_4"] : null;
-	$tipo_act_4 = $_POST["tipo_4"] != "" ? $_POST["tipo_4"] : null;
-	$hora_act_4 = $_POST["hora_act_4"] != "" ? $_POST["hora_act_4"] : null;
+	$id_act_4 = $_POST["id_4"];
+	$tipo_act_4 = $_POST["tipo_4"];
+	$hora_act_4 = $_POST["hora_act_4"];
 
-	$id_act_5 = $_POST["id_5"] != "" ? $_POST["id_5"] : null;
-	$tipo_act_5 = $_POST["tipo_5"] != "" ? $_POST["tipo_5"] : null;
-	$hora_act_5 = $_POST["hora_act_5"] != "" ? $_POST["hora_act_5"] : null;
+	$id_act_5 = $_POST["id_5"];
+	$tipo_act_5 = $_POST["tipo_5"];
+	$hora_act_5 = $_POST["hora_act_5"];
 
 	$fecha = $_POST["fecha"] != "" ? $_POST["fecha"] : null;
 
@@ -637,24 +638,24 @@ else{
 
 	}
 	if($action === "update"){
-		if($nombre)$plan->setNombre($nombre);
-		if($descripcion)$plan->setDescripcion($descripcion);
-		if($hora_act_1)$plan->setHora_act_1($hora_act_1);
-		if($id_act_1)$plan->setId_act_1($id_act_1);
-		if($tipo_act_1)$plan->setTipo_act_1($tipo_act_1);
-		if($hora_act_2)$plan->setHora_act_2($hora_act_2);
-		if($id_act_2)$plan->setId_act_2($id_act_2);
-		if($tipo_act_2)$plan->setTipo_act_2($tipo_act_2);
-		if($hora_act_3)$plan->setHora_act_3($hora_act_3);
-		if($id_act_3)$plan->setId_act_3($id_act_3);
-		if($tipo_act_3)$plan->setTipo_act_3($tipo_act_3);
-		if($hora_act_4)$plan->setHora_act_4($hora_act_4);
-		if($id_act_4)$plan->setId_act_4($id_act_4);
-		if($tipo_act_4)$plan->setTipo_act_4($tipo_act_4);
-		if($hora_act_5)$plan->setHora_act_5($hora_act_5);
-		if($id_act_5)$plan->setId_act_5($id_act_5);
-		if($tipo_act_5)$plan->setTipo_act_5($tipo_act_5);
+		if($nombre){$plan->setNombre($nombre);}
+		if($descripcion){$plan->setDescripcion($descripcion);}
 		if($fecha)$plan->setFecha($fecha);
+		$plan->setHora_act_1($hora_act_1);
+		$plan->setId_act_1($id_act_1);
+		$plan->setTipo_act_1($tipo_act_1);
+		$plan->setHora_act_2($hora_act_2);
+		$plan->setId_act_2($id_act_2);
+		$plan->setTipo_act_2($tipo_act_2);
+		$plan->setHora_act_3($hora_act_3);
+		$plan->setId_act_3($id_act_3);
+		$plan->setTipo_act_3($tipo_act_3);
+		$plan->setHora_act_4($hora_act_4);
+		$plan->setId_act_4($id_act_4);
+		$plan->setTipo_act_4($tipo_act_4);
+		$plan->setHora_act_5($hora_act_5);
+		$plan->setId_act_5($id_act_5);
+		$plan->setTipo_act_5($tipo_act_5);
 
 		if($plan->update())
 			header("location: ../dashboard/actividadPage.php?content=plan&id=$id");
